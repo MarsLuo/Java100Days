f862fce969f3cc8a03910b1634579718a05def63
LuoYang <lyyx_0@163.com>
2021-04-11 22:57:22 +0800

    use join point

--- spring/Spring-06-aspectj/src/main/java/org/example/ba01/MyAspect.java
+++ spring/Spring-06-aspectj/src/main/java/org/example/ba01/MyAspect.java
@@ -1,5 +1,6 @@
 package org.example.ba01;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -8,8 +9,21 @@ import java.util.Date;
 @Aspect
 public class MyAspect {
 
+//    @Before(value = "execution(public void org.example.ba01.SomeServiceImpl.doSome(String, Integer))")
+//    public void myBefore() {
+//        System.out.println("new Date: "+ new Date());
+//    }
+
     @Before(value = "execution(public void org.example.ba01.SomeServiceImpl.doSome(String, Integer))")
-    public void myBefore() {
+    public void myBefore(JoinPoint jp)  {
+        System.out.println("Signature: "+jp.getSignature());
+        System.out.println("method name:"+jp.getSignature().getName());
+
+        Object args [] = jp.getArgs();
+        for (Object arg : args) {
+            System.out.println("param: "+arg);
+        }
+
         System.out.println("new Date: "+ new Date());
     }
 
