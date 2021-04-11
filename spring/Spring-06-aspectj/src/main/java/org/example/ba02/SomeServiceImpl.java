70d024bb25ddc084e2a2bfa92937ebb6c33c729a
LuoYang <lyyx_0@163.com>
2021-04-11 23:05:30 +0800

    update project

---
+++ spring/Spring-06-aspectj/src/main/java/org/example/ba02/SomeServiceImpl.java
@@ -0,0 +1,8 @@
+package org.example.ba02;
+
+public class SomeServiceImpl implements SomeService {
+    @Override
+    public void doSome(String name, Integer age) {
+        System.out.println("SomeServiceImpl doSome");
+    }
+}
--- spring/Spring-06-aspectj/src/test/java/org/example/Test02.java
+++ spring/Spring-06-aspectj/src/test/java/org/example/Test02.java
@@ -1,15 +1,15 @@
 package org.example;
 
-import org.example.ba01.SomeService;
+import org.example.ba02.SomeService;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Test01 {
+public class Test02 {
 
     @Test
     public void tese01() {
-        String config = "applicationContext-01.xml";
+        String config = "applicationContext-02.xml";
         ApplicationContext context = new ClassPathXmlApplicationContext(config);
         SomeService proxy = (SomeService) context.getBean("someService");
 
