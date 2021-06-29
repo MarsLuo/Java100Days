028115a269056ef36217dd4b0f02e6385aac4f69
LuoYang <lyyx_0@163.com>
2021-06-29 17:08:54 +0800

    add controller

--- spring/hello-spring/src/main/java/com/marsluo/hello/hellospring/HelloSpringApplication.java
+++ spring/hello-spring/src/main/java/com/marsluo/hello/hellospring/HelloSpringApplication.java
@@ -6,15 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class HelloSpringApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringApplication.class, args);
-	}
-
-	@RequestMapping("/hello")
-	public String hello() {
-		return "Hello Spring";
 	}
 }
--- spring/hello-spring/src/test/java/com/marsluo/hello/hellospring/HelloSpringApplicationTests.java
+++ spring/hello-spring/src/test/java/com/marsluo/hello/hellospring/HelloSpringApplicationTests.java
@@ -1,13 +1,29 @@
 package com.marsluo.hello.hellospring;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+
 class HelloSpringApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string("Hello Spring"));
 	}
 
 }
