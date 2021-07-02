dbf00e6af8197e6be6d02312e6a93bf62cbdba58
LuoYang <lyyx_0@163.com>
2021-07-02 12:13:00 +0800

    add swagger support

--- spring/hello-spring/src/main/java/com/marsluo/hello/hellospring/controller/HelloController.java
+++ spring/hello-spring/src/main/java/com/marsluo/hello/hellospring/controller/HelloController.java
@@ -1,10 +1,11 @@
 package com.marsluo.hello.hellospring.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
         return "Hello Spring";
     }
--- spring/hello-spring/src/main/java/com/marsluo/hello/hellospring/controller/UserController.java
+++ spring/hello-spring/src/main/java/com/marsluo/hello/hellospring/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.marsluo.hello.hellospring.controller;
 
 import com.marsluo.hello.hellospring.model.User;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
@@ -10,24 +13,33 @@ import java.util.*;
 public class UserController {
     static Map<Long, User> users = Collections.synchronizedMap(new HashMap<>());
 
+    @ApiOperation(value = "获取用户列表")
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public List<User> getUserList() {
         return new ArrayList<>(users.values());
     }
 
     //get
+    @ApiOperation(value="创建用户", notes="根据User对象创建用户")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public User getUser(@PathVariable Long id) {
         return  users.get(id);
     }
 
     //post
+    @ApiOperation(value="获取用户详细信息", notes="根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public String postUser(@ModelAttribute User user) {
         users.put(user.getId(), user);
         return "success";
     }
     //put
+    @ApiOperation(value="更新用户详细信息", notes="根据url的id来指定更新对象，并根据传过来的user信息来更新用户详细信息")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long"),
+            @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+    })
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public String putUser(@PathVariable Long id, @ModelAttribute User user) {
         User beforeUser = users.get(id);
@@ -37,6 +49,8 @@ public class UserController {
         return "success";
     }
     //delete
+    @ApiOperation(value="删除用户", notes="根据url的id来指定删除对象")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public User delUser(@PathVariable Long id) {
         return  users.remove(id);
